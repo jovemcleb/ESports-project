a061c891c1d6f6b23c1f64a99d6aa85b43aec06c
Caleb Lima <lima.cardoso.caleb@gmail.com>
2022-09-19 21:44:23 -0300

    :sparkles: feat: adiciona componentes Loading e Home ao App

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,28 @@
-import { View } from 'react-native';
+import { StatusBar } from 'react-native';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+} from '@expo-google-fonts/inter';
+
+import { Home } from './src/screens/home';
+import { Background } from './src/components/background';
+import { Loading } from './src/components/loading';
 
 export default function App() {
-  return (
-    <View>
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  });
 
-    </View>
+  return (
+    <Background>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+      {fontsLoaded ? <Home /> : <Loading />}
+    </Background>
   );
 }

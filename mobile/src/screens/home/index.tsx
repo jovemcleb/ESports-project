4c7db251ec9f9d839e80b61a265e92c91ca5371b
Caleb Lima <lima.cardoso.caleb@gmail.com>
2022-09-19 22:27:34 -0300

    :sparkles: feat: adiciona componente heading a Home

--- mobile/src/screens/home/index.tsx
+++ mobile/src/screens/home/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image } from 'react-native';
+import { Heading } from '../../components/heading';
+
+import logoImg from './../../assets/logo-nlw-esports.png'
 
 import { styles } from './styles';
 
 export function Home() {
   return (
     <View style={styles.container}>
+      <Image source={logoImg} style={styles.logo} />
 
+      <Heading title='Encontre seu Duo!' subtitle='Selecione o game que deseja jogar...' />
     </View>
   );
 }

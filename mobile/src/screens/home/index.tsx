856ee127c91678386b2609087b7f92904a4f407d
Caleb Lima <lima.cardoso.caleb@gmail.com>
2022-09-26 18:22:52 -0300

    :sparkles: feat: adiciona FlatList de games a home

--- mobile/src/screens/home/index.tsx
+++ mobile/src/screens/home/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { View, Image } from 'react-native';
-import { Heading } from '../../components/heading';
+import { View, Image, FlatList } from 'react-native';
 
 import logoImg from './../../assets/logo-nlw-esports.png'
+import { GameCard } from '../../components/gameCard';
+import { Heading } from '../../components/heading';
 
+import { GAMES } from '../../utils/games';
 import { styles } from './styles';
 
 export function Home() {
@@ -12,6 +14,15 @@ export function Home() {
       <Image source={logoImg} style={styles.logo} />
 
       <Heading title='Encontre seu Duo!' subtitle='Selecione o game que deseja jogar...' />
+
+      <FlatList
+        contentContainerStyle={styles.contentList}
+        data={GAMES}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (<GameCard data={item} />)}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+      />
     </View>
   );
 }
